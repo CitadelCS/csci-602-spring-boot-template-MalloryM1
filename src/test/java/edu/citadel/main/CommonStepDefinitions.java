8953761b11bd80316d90ada57d2c3dbfa7adcf7e
MalloryMease <measem21@gmail.com>
2025-10-08 13:32:17 -0400

    Cucumber tests added

---
+++ src/test/java/edu/citadel/main/CommonStepDefinitions.java
@@ -0,0 +1,26 @@
+package edu.citadel.main;
+
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.And;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CommonStepDefinitions extends SpringIntegrationTest {
+
+    @When("the client calls /version")
+    public void theClientCallsVersion() {
+        String url = createURLWithPort("/version");
+        execute(url);
+    }
+
+    @Then("the client receives status code of {int}")
+    public void theClientReceivesStatusCodeOf(int statusCode) {
+        assertEquals(statusCode, lastResponse.getStatusCode().value());
+    }
+
+    @And("the client receives server version {double}")
+    public void theClientReceivesServerVersion(double version) {
+        assertEquals(String.valueOf(version), lastResponse.getBody());
+    }
+}
---
+++ src/test/java/edu/citadel/main/SpringIntegrationTest.java
@@ -0,0 +1,26 @@
+package edu.citadel.main;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+@CucumberContextConfiguration
+@SpringBootTest
+public class SpringIntegrationTest {
+    // executeGet implementation
+    @LocalServerPort
+    private int port;
+
+    protected TestRestTemplate testRestTemplate = new TestRestTemplate();
+    protected ResponseEntity<String> lastResponse;
+
+    protected void execute(String uri) {
+        lastResponse = testRestTemplate.getForEntity(uri, String.class);
+    }
+
+    protected String createURLWithPort(String uri) {
+        return "http://localhost:" + port + uri;
+    }
+}
