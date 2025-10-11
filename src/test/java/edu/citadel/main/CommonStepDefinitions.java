bc7408a62801040d27f2a3c4e3bf011d5186d5ed
MalloryMease <measem21@gmail.com>
2025-10-10 23:53:25 -0400

    push

--- src/test/java/edu/citadel/main/CommonStepDefinitions.java
+++ src/test/java/edu/citadel/main/CommonStepDefinitions.java
@@ -5,12 +5,25 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.And;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CommonStepDefinitions extends SpringIntegrationTest {
 
-    @When("the client calls /version")
+    @When("the client calls \\/version")
     public void theClientCallsVersion() {
         String url = createURLWithPort("/version");
+        execute(url);
+    }
+
+    @When("the client calls \\/health")
+    public void theClientCallsHealth() {
+        String url = createURLWithPort("/health");
+        execute(url);
+    }
+
+    @When("the client calls \\/info")
+    public void theClientCallsInfo() {
+        String url = createURLWithPort("/info");
         execute(url);
     }
 
@@ -22,5 +35,20 @@ public class CommonStepDefinitions extends SpringIntegrationTest {
     @And("the client receives server version {double}")
     public void theClientReceivesServerVersion(double version) {
         assertEquals(String.valueOf(version), lastResponse.getBody());
+    }
+
+    @And("the client receives health status ok")
+    public void theClientReceivesHealthStatusOk() {
+        String responseBody = lastResponse.getBody();
+        assertTrue(responseBody.contains("\"status\""));
+        assertTrue(responseBody.contains("\"ok\""));
+    }
+
+    @And("the client receives application info")
+    public void theClientReceivesApplicationInfo() {
+        String responseBody = lastResponse.getBody();
+        assertTrue(responseBody.contains("\"name\""));
+        assertTrue(responseBody.contains("\"version\""));
+        assertTrue(responseBody.contains("\"description\""));
     }
 }
--- src/test/resources/version.feature
+++ src/test/resources/version.feature
@@ -1,5 +1,15 @@
 Feature: the version can be retrieved
   Scenario: client makes call to GET /version
-    When the client calls /version
-    Then the client receives status code of 200
-    And the client receives server version 1.0
+  When the client calls /version
+  Then the client receives status code of 200
+  And the client receives server version 1.0
+
+  Scenario: client makes call to GET /health
+  When the client calls /health
+  Then the client receives status code of 200
+  And the client receives health status ok
+
+  Scenario: client makes call to GET /info
+  When the client calls /info
+  Then the client receives status code of 200
+  And the client receives application info
